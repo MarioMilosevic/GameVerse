d69bb24d931a2affbf38711648dd0be47931927c
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-02-22 12:53:08 +0100

    Install dependencies to server

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import usersRouter from "./routes/users";
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use("/api/v1/users", usersRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server running");
+});
+
+
+// app.all("*", (req: Request, res: Response, next: NextFunction) => {
+//   errorFactory.notFound(res);
+// });
+
+export default app;
