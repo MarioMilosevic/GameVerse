ea781a768b1d68b5f871cce8928b1faad563bc05
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-02-26 21:31:57 +0100

    Add icons

--- client/src/components/GameComponent.vue
+++ client/src/components/GameComponent.vue
@@ -1,20 +1,21 @@
 <template>
-  <li class="border border-black h-[500px] w-[300px]"> 
-    <img :src="game.thumbnail" class="w-full"/>
+  <li class="border border-black h-[500px] w-[300px] flex flex-col gap-2">
+    <img :src="game.thumbnail" class="w-full" />
     <h2 class="text-center">{{ game.name }}</h2>
-    <p>{{ game.description }}</p>
+    <!-- <p>{{ game.description }}</p> -->
   </li>
 </template>
 
 <script setup lang="ts">
 import { PropType } from "vue";
 import { GameType } from "src/utils/types";
+
 const props = defineProps({
   game: {
-        type: Object as PropType<GameType>,
-        required:true
+    type: Object as PropType<GameType>,
+    required: true,
   },
 });
 
-console.log(props.game)
+console.log(props.game);
 </script>
---
+++ client/src/icons/BaseIcon.vue
@@ -0,0 +1,53 @@
+<template>
+  <svg
+    :xmlns="xmlns"
+    :fill="fill"
+    :viewBox="viewBox"
+    :stroke-width="strokeWidth"
+    :stroke="stroke"
+    :class="[`icon-${props.size}`]"
+    :style="{ cursor }"
+    :size="size"
+  >
+    <slot />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  xmlns: {
+    type: String,
+    default: 'http://www.w3.org/2000/svg',
+  },
+  fill: {
+    type: String,
+    default: 'none',
+  },
+  viewBox: {
+    type: String,
+    default: '0 0 24 24',
+  },
+  strokeWidth: {
+    type: String,
+    default: '1.5',
+  },
+  stroke: {
+    type: String,
+    default: 'currentColor',
+  },
+  size: {
+    type: String,
+    default: 'medium',
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const cursor = computed(() => {
+  return props.isDisabled ? 'not-allowed' : 'pointer'
+})
+</script>
