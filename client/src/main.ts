d716eab4d70231a6e2abe3830626c4b6fb06aab6
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-03-10 17:33:39 +0100

    Add vue youtube player

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,10 +4,13 @@ import router from "src/router";
 import "./style.css";
 import App from "./App.vue";
 import Vue3Toastify, { ToastContainerOptions } from "vue3-toastify";
+// import VueVideoPlayer from "@videojs-player/vue"
+// import 'video.js/dist/video-js.css'
 
 createApp(App)
   .use(router)
   .use(createPinia())
+  // .use(VueVideoPlayer)
   .use(Vue3Toastify, {
     autoClose: 3000,
   } as ToastContainerOptions)
--- client/src/views/SingleGameView.vue
+++ client/src/views/SingleGameView.vue
@@ -1,24 +1,50 @@
 <template>
-  <h1>Ovo je singl stranica</h1>
+  <LoadingSpinner v-if="loading" />
+  <main v-else class="border border-white mx-auto">
+    <h1>Ovo je singl stranica</h1>
+    <YouTube :src="singleGame.trailer" ref="youtube" @ready="playVideo" />
+  </main>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import { getSingleGame } from "src/api/games";
-import { onBeforeMount, ref } from "vue";
+import { onBeforeMount, ref, useTemplateRef } from "vue";
 import { GameType } from "src/utils/types";
 import { showToast } from "src/utils/toast";
+import { useLoadingStore } from "src/stores/loadingStore";
+import { storeToRefs } from "pinia";
+import LoadingSpinner from "src/components/layout/LoadingSpinner.vue";
+import YouTube from "vue3-youtube";
 
-const route = useRoute();
 onBeforeMount(async () => {
-  const { message, data } = await getSingleGame(Number(route.params.id));
-  if (message) {
-    showToast(message, "error");
-  } else {
-    console.log(data);
-    singleGame.value = data;
+  try {
+    const { message, data } = await getSingleGame(Number(route.params.id));
+    if (message) {
+      showToast(message, "error");
+    } else {
+      console.log(data);
+      singleGame.value = data;
+    }
+  } catch (error) {
+    showToast("Unexpected error occured", 'error')
+    console.error(error);
+  } finally {
+    loadingStore.setLoading(false);
   }
 });
 
-const singleGame = ref<GameType>();
+const loadingStore = useLoadingStore();
+const { loading } = storeToRefs(loadingStore);
+
+const route = useRoute();
+const youtubeRef = useTemplateRef("youtube");
+
+const singleGame = ref<GameType>({} as GameType);
+
+const playVideo = () => {
+  if (youtubeRef.value) {
+    youtubeRef.value.playVideo();
+  }
+};
 </script>
