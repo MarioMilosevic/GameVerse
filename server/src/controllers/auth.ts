92b632fcad868acfe58a1599953228679b33ce2a
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-03-17 12:18:59 +0100

    Install bcrypt and jwt for authorization

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -3,18 +3,30 @@ import errorFactory from "../services/responses/errorFactory"
 import sucessFactory from "../services/responses/sucessFactory";
 import prisma from "../../prisma/prismaClient";
 import validator from 'validator';
+import jwt from 'jsonwebtoken';
+import config from '../config'
+
+// const signToken = (id: number) =>
+//   jwt.sign({ id }, config.secrets.jwt, {
+//     expiresIn: config.secrets.expiresIn,
+//   });
+
 
 export default {
   async signUp(req: Request, res: Response) {
-    try {
-      const user = await prisma.user.create({
-        data: req.body,
-      });
-      if (!user) {
-        errorFactory.badRequest(res);
-        return;
-      }
-      sucessFactory.created(res, user);
+        try {
+        
+            const {fullName, email, password, passwordConfirm, role, image} = req.body
+            console.log(fullName, email, password, passwordConfirm, role, image)
+    //   const user = await prisma.user.create({
+    //     data: req.body,
+    //   });
+    //   if (!user) {
+    //     errorFactory.badRequest(res);
+    //     return;
+    //   }
+      sucessFactory.created(res, 'proslo');
+    //   sucessFactory.created(res, user);
     } catch (error) {
       errorFactory.internalError(res);
     }
