8d6f2be89897cbe72c7d82119def3bfe6b62be8a
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-04-18 11:26:59 +0200

    Disable pagination buttons

--- client/src/components/layout/dashboard/PaginationComponent.vue
+++ client/src/components/layout/dashboard/PaginationComponent.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="flex items-center justify-between border p-4 mt-12 rounded-xl">
+  <div class="flex items-center justify-between mt-4 rounded-xl">
     <p v-if="users > 0" class="text-lg">
       Showing {{ showResultsFrom }} to {{ showResultsTo }} out of
       {{ props.users }}
     </p>
     <p v-else>No users found</p>
     <div class="flex gap-4">
-      <ActionButton @click="emits('previous-event')">Previous</ActionButton>
-      <ActionButton @click="emits('next-event')">Next</ActionButton>
+      <ActionButton
+        :disabled="props.currentPage === 1"
+        @click="emits('previous-event')"
+        >Previous</ActionButton
+      >
+      <ActionButton
+        :disabled="!(props.currentPage < users / usersPerPage)"
+        @click="emits('next-event')"
+        >Next</ActionButton
+      >
     </div>
   </div>
 </template>
@@ -24,20 +32,18 @@ const props = defineProps({
   },
   currentPage: {
     type: Number,
-    required:true
-  }
+    required: true,
+  },
 });
 
 const showResultsFrom = computed(() => {
-  return (props.currentPage - 1) * usersPerPage + 1
-})
-
-const showResultsTo = computed(() => {
-  const to = showResultsFrom.value + usersPerPage - 1
-  return to > props.users ? props.users : to
+  return (props.currentPage - 1) * usersPerPage + 1;
 });
 
+const showResultsTo = computed(() => {
+  const to = showResultsFrom.value + usersPerPage - 1;
+  return to > props.users ? props.users : to;
+});
 
-const emits = defineEmits(['previous-event', 'next-event'])
-
+const emits = defineEmits(["previous-event", "next-event"]);
 </script>
